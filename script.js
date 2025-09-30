16819885d33a0e3db3e1e8699500493fb9065904
Lorena Milani <lorena.milani.santos@escola.pr.gov.br>
2025-09-30 07:46:02 -0300

    aula 30/09

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-const avanca =document.querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button =>{
     button.addEventListener('click', function (){
-        const atual =document.querySelectorAll('.ativo');
+        const atual = document.querySelectorAll('.ativo');
         const proximoPasso = 'passo-' + this. getAttribute('data-proximo ');
 
         atual.classList.remove('ativo');
         document.getElementById(proximoPasso).classList.add('ativo');
-        
-    }
-
-    )
-}
-
-)
+    } )
+})
